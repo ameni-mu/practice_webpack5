d4278c5d7475c82070dcaeaf5cabb5e716f051e2
ameni-mu <info@atlier-ameni.com>
2021-06-07 13:38:44 +0900

    init

---
+++ src/index.js
@@ -0,0 +1,5 @@
+import Main from './js/main.js';
+import reset from './scss/reset.scss';
+import style from './scss/style.scss';
+
+new Main();
---
+++ src/js/main.js
@@ -0,0 +1,11 @@
+export default class Main {
+  ///////////////////////////
+  /// constructor
+  ////////////////////////////
+  constructor(){
+
+    console.log('test');
+
+  }
+
+}
